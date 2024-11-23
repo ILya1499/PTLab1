71721b58b6242adc53b8c4d83d1706cf49d4653a
Efremenkov Ilya Evgenievich <IEfremenkov@gmail.com>
2024-11-23 21:16:14 +0300

    My first commit

--- test/text_TextDataReader.py
+++ test/text_TextDataReader.py
@@ -21,7 +21,7 @@ class TestTextDataReader:
                 ]
         }
         return text, data
-        
+
     @pytest.fixture()
     def filepath_and_data(self,
                           file_and_data_content: tuple[str, DataType],
@@ -29,7 +29,7 @@ class TestTextDataReader:
         p = tmpdir.mkdir("datadir").join("my_data.txt")
         p.write_text(file_and_data_content[0], encoding='utf-8')
         return str(p), file_and_data_content[1]
-        
+
     def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
         file_content = TextDataReader().read(filepath_and_data[0])
         assert file_content == filepath_and_data[1]
