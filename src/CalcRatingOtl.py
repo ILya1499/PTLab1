b52adbf40fc35a79d3b5c20c6b46d57b3c04fa6a
ILya1499 <113355089+ILya1499@users.noreply.github.com>
2024-12-12 13:24:51 +0300

    end

--- src/CalcRatingOtl.py
+++ src/CalcRatingOtl.py
@@ -3,15 +3,17 @@ from Types import DataType
 RatingType = dict[str, float]
 
 
-class CalcRating:
+class CalcRatingOtl:
     def __init__(self, data: DataType) -> None:
         self.data: DataType = data
         self.rating: RatingType = {}
 
-    def calc(self) -> RatingType:
+    def so(self) -> str:
         for key in self.data:
-            self.rating[key] = 0.0
+            current = 0
             for subject in self.data[key]:
-                self.rating[key] += subject[1]
-            self.rating[key] /= len(self.data[key])
-        return self.rating
+                if subject[1] >= 76:
+                    current = current + 1
+                if current >= 3:
+                    return key
+        return "Студентов не найдено"
